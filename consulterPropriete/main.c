84bb371744f766df9a1ee41c493171b66571af79
Hugo MAZERAT <hugo.mazerat@edu.ece.fr>
2022-01-08 16:56:25 +0100

    Envoi de nvx codes

---
+++ consulterPropriete/main.c
@@ -0,0 +1,12 @@
+#include "maBiblio.h"
+
+
+
+int main()
+{
+    joueur tableauj[4];
+    int nbj;//Nombre de joueurs.
+    consulterPropriete(tableauj, nbj);
+    printf("Hello world!\n");
+    return 0;
+}
